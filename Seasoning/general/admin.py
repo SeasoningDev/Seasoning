7c8707803e54e71c21f1888959b3c8ac33f1a0d5
Joep Driesen <joep.driesen@seasoning.be>
2013-12-10 18:45:10 +0100

    stuffs

--- Seasoning/general/admin.py
+++ Seasoning/general/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from general.models import StaticPage
+from general.models import StaticPage, RecipeOfTheWeek
 
 admin.site.register(StaticPage)
+admin.site.register(RecipeOfTheWeek)
