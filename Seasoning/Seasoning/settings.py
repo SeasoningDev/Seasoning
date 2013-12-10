a3f0e47a113ba3b7ef81b3b4e51ac121ecd48a08
Joep Driesen <joep.driesen@seasoning.be>
2013-12-10 17:51:27 +0100

    added debug toolbar

--- Seasoning/Seasoning/settings.py
+++ Seasoning/Seasoning/settings.py
@@ -130,6 +130,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'Seasoning.urls'
@@ -170,6 +171,7 @@ INSTALLED_APPS = (
     'imagekit',
     'pipeline',
     'markitup',
+    'debug_toolbar',
 )
 
 AUTH_USER_MODEL = 'authentication.User'
@@ -327,4 +329,25 @@ PIPELINE_JS_COMPRESSOR = 'pipeline.compressors.jsmin.JSMinCompressor'
 MARKITUP_FILTER = ('markdown.markdown', {'safe_mode': True})
 
 JQUERY_URL = None
+
+INTERNAL_IPS = ('0.0.0.0',)
+
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
+def show_toolbar(request):
+    return True
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': show_toolbar
+}
+
 
