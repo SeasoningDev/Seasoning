01af1c2309ee5d088a4b525824993bb39e7b16ac
Joep <Joep@Gargamel.home>
2014-02-17 17:44:38 +0100

    Fixed Issue #54

--- Seasoning/recipes/admin.py
+++ Seasoning/recipes/admin.py
@@ -7,6 +7,17 @@ class UsesIngredientInline(admin.TabularInline):
 class RecipeAdmin(admin.ModelAdmin):
     inlines = [ UsesIngredientInline, ]
     
+    # Make sure the recipe object gets saved again after the usesingredient objects are saved
+    temp_obj = None
+    
+    def save_model(self, request, obj, form, change):
+        self.temp_obj = obj
+        admin.ModelAdmin.save_model(self, request, obj, form, change)
+    
+    def save_related(self, request, form, formsets, change):
+        admin.ModelAdmin.save_related(self, request, form, formsets, change)
+        self.temp_obj.save()
+    
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(Cuisine)
 admin.site.register(UnknownIngredient)
