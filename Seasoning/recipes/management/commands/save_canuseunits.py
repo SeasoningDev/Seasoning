c4ae7e34b9491753531a2226bbddb70d52088b7b
Joep <Joep@Gargamel.home>
2014-03-14 00:54:29 +0100

    Fix for Issue #116

---
+++ Seasoning/recipes/management/commands/save_canuseunits.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from ingredients.models import CanUseUnit
+
+class Command(BaseCommand):
+    def handle(self):
+        """
+        This command is only here for the transition of the old useable_units system to the new one
+        
+        It saves all canuseunits so that all child unit will also be added to ingredients made before
+        the change
+        
+        """
+        for canuseunit in CanUseUnit.objects.all():
+            canuseunit.save()
