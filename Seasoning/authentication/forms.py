8a91edc427d03cd6425b4dd75889294e01327f7e
Joep <Joep@Gargamel.home>
2014-03-19 02:57:37 +0100

    bf

--- Seasoning/authentication/forms.py
+++ Seasoning/authentication/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.forms import AuthenticationForm, PasswordResetForm
@@ -197,15 +197,22 @@ class CheckActiveAuthenticationForm(AuthenticationForm):
                                                       'after registering, you can use <a href=\"/activate/resend/\">this form</a> '
                                                       'to resend an activation email.'))
         self.error_messages['invalid_login_override'] = _("Override: Please enter a correct email address and password. "
-                                                          "Note that both fields may be case-sensitive."),
+                                                          "Note that both fields may be case-sensitive.")
 
     def clean(self):
-        try:
-            super(CheckActiveAuthenticationForm, self).clean()
-        except forms.ValidationError as e:
-            if self.error_messages['invalid_login'] in e.messages:
-                raise forms.ValidationError(self.error_messages['invalid_login_override'])
-            raise e
+        username = self.cleaned_data.get('username')
+        password = self.cleaned_data.get('password')
+
+        if username and password:
+            self.user_cache = authenticate(username=username,
+                                           password=password)
+            if self.user_cache is None:
+                raise forms.ValidationError(
+                    self.error_messages['invalid_login_override'])
+            elif not self.user_cache.is_active:
+                raise forms.ValidationError(self.error_messages['inactive'])
+        self.check_for_test_cookie()
+        return self.cleaned_data
         
 class AccountSettingsForm(ModelForm):
     """
