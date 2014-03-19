3938a980bbe4cad03ee78d674801f335730a9d2f
Joep <Joep@Gargamel.home>
2014-03-19 02:13:48 +0100

    try

---
+++ Seasoning/authentication/django_standard_messages.py
@@ -0,0 +1,5 @@
+# coding: utf-8
+_ = lambda s: s
+django_standard_messages_to_override = [
+    _("Please enter the correct %(username)s and password for a staff account. Note that both fields may be case-sensitive."),
+]
