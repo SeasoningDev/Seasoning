e5cb5fb9499e4301447e710c9510e5625c6106a7
Joep <Joep@Gargamel.home>
2014-03-20 18:43:54 +0100

    Fixed Issue #77, Issue #131 and Issue #134

--- Seasoning/general/__init__.py
+++ Seasoning/general/__init__.py
@@ -3,7 +3,10 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 
 def validate_image_size(fieldfile_obj):
-    file_size = fieldfile_obj.file.size
+    try:
+        file_size = fieldfile_obj.file.size
+    except IOError:
+        return
     size_limit = settings.MAX_UPLOAD_SIZE
     if file_size > size_limit:
         raise ValidationError(_('Max file size is %sMB') % str(size_limit/(1024*1024)))
