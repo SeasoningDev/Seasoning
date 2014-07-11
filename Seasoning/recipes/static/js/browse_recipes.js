ec1846a7969131db5c6a660e7e9b4a540310f30f
Joep Driesen <joep.driesen@seasoning.be>
2014-07-11 19:52:11 +0200

    Fixed Issue #227

--- Seasoning/recipes/static/js/browse_recipes.js
+++ Seasoning/recipes/static/js/browse_recipes.js
@@ -192,7 +192,7 @@ $(document).ready(function() {
 	 */
 	// Start the timer when more than 3 chars have been typed
 	$("#id_search_string").keyup(function(event) {
-		if (event.keyCode >= 65 && event.keyCode <= 90) {
+		if ((event.keyCode >= 65 && event.keyCode <= 90) || event.keyCode == 8) {
 			var x = $(this).val().length;
 			clearTimeout(timer);
 			timer = setTimeout(update_recipe_page, 100);
