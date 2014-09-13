97894d3a78b971bbbfa399c23ad420a89deb0454
joep <joep@localhost.localdomain>
2014-09-13 17:19:45 +0200

    Fixed Issue #270

--- Seasoning/news/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-
-urlpatterns = patterns('',
-    url(r'^$', 'news.views.browse_news', name='browse_news'),
-    url(r'^(\d*)/$', 'news.views.view_news', name='view_news'),
-)
