28dc530664ecdb43c8c5a31b0efb340caa52385d
joep <joep@localhost.localdomain>
2014-10-06 12:50:20 +0200

    Fixed Issue #277

--- Seasoning/general/__init__.py
+++ Seasoning/general/__init__.py
@@ -3,6 +3,10 @@ from django.core.exceptions import ValidationError
 from django.utils.translation import ugettext_lazy as _
 import re
 import hashlib
+from django.template import loader
+from django.core.mail.message import EmailMultiAlternatives
+import os
+from email.mime.image import MIMEImage
 
 def validate_image_size(fieldfile_obj):
     try:
@@ -14,7 +18,6 @@ def validate_image_size(fieldfile_obj):
         raise ValidationError(_('Max file size is %sMB') % str(size_limit/(1024*1024)))
 
 def all_templates(filter_with='', filter_without=''):
-    import os
     from django.template.loaders.app_directories import app_template_dirs
     
     tid = 0
@@ -37,3 +40,30 @@ def all_templates(filter_with='', filter_without=''):
                 tid += 1
                                   
     return templates
+
+def send_seasoning_email(subject_template, text_template, html_template, template_context,
+                         from_email, to_emails):
+    subject = loader.render_to_string(subject_template, template_context)
+    
+    # Email subject *must not* contain newlines
+    subject = ''.join(subject.splitlines())
+    
+    # Render message templates
+    message_text = loader.render_to_string(text_template, template_context)
+    message_html = loader.render_to_string(html_template, template_context)            
+    
+    
+    msg = EmailMultiAlternatives(subject, message_text, from_email, to_emails)
+    # Attach html message
+    msg.attach_alternative(message_html, "text/html")
+    
+    msg.mixed_subtype = 'related'
+
+    # Embed seasoning logo in email
+    fp = open(settings.SMALL_LOGO_FILE, 'rb')
+    msg_img = MIMEImage(fp.read())
+    fp.close()
+    msg_img.add_header('Content-ID', '<{}>'.format('logo.png'))
+    msg.attach(msg_img)
+    
+    msg.send()
