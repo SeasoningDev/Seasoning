7a616004e18683d2f2d5be407c72d19176ae90b9
Joep Driesen <joep.driesen@seasoning.be>
2015-04-01 11:36:16 +0200

    search update

--- Seasoning/recipes/static/js/browse_recipes.js
+++ Seasoning/recipes/static/js/browse_recipes.js
@@ -2,6 +2,10 @@
  * This timer will be reset after the user has typed his last character
  * in the search field. This prevents a buttload of queries when a user is
  * search for a long string.
+ * 
+ * Form and filter stuff courtesy of Codyhouse Content Filter Plugin
+ * http://codyhouse.co/gem/content-filter/
+ * 
  */
 var timer;
 
@@ -10,6 +14,65 @@ function update_recipe_page() {
 }
 
 $(document).ready(function() {
+	
+
+	// Open and Close lateral filter (Codyhouse)
+	$('.cd-filter-trigger').on('click', function(){
+		triggerFilter(true);
+	});
+	$('.cd-filter .cd-close').on('click', function(){
+		triggerFilter(false);
+	});
+
+	function triggerFilter($bool) {
+		var elementsToTrigger = $([$('.cd-filter-trigger'), $('.cd-filter'), $('.cd-tab-filter'), $('.cd-gallery')]);
+		elementsToTrigger.each(function(){
+			$(this).toggleClass('filter-is-visible', $bool);
+		});
+	}
+	
+	
+
+
+	// Detect click event on sorting button (Codyhouse)
+	var filter_tab_placeholder = $('.cd-tab-filter .placeholder a'),
+		filter_tab_placeholder_default_value = 'Sorteren op...',
+		filter_tab_placeholder_text = filter_tab_placeholder.text();
+	
+	$('.cd-tab-filter li').on('click', function(event){
+		//detect which tab filter item was selected
+		var selected_filter = $(event.target).data('type');
+			
+		//check if user has clicked the placeholder item
+		if( $(event.target).is(filter_tab_placeholder) ) {
+			(filter_tab_placeholder_default_value == filter_tab_placeholder.text()) ? filter_tab_placeholder.text(filter_tab_placeholder_text) : filter_tab_placeholder.text(filter_tab_placeholder_default_value) ;
+			$('.cd-tab-filter').toggleClass('is-open');
+
+		//check if user has clicked a filter already selected 
+		} else if( filter_tab_placeholder.data('type') == selected_filter ) {
+			filter_tab_placeholder.text($(event.target).text());
+			$('.cd-tab-filter').removeClass('is-open');	
+
+		} else {
+			//close the dropdown and change placeholder text/data-type value
+			$('.cd-tab-filter').removeClass('is-open');
+			filter_tab_placeholder.text($(event.target).text()).data('type', selected_filter);
+			filter_tab_placeholder_text = $(event.target).text();
+			
+			//add class selected to the selected filter item
+			$('.cd-tab-filter .selected').removeClass('selected');
+			$(event.target).addClass('selected');
+		}
+	});
+	
+	
+	
+
+	// Close a filter group dropdown inside lateral .cd-filter 
+	$('.cd-filter-block h4').on('click', function(){
+		$(this).toggleClass('closed').siblings('.cd-filter-content').slideToggle(300);
+	})
+	
 	
 	$("#id_sort_field").click(function(e) {
 		e.stopPropagation();
