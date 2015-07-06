fe7d9979bbb948bfac41c8d14b3cff964a2c3ce6
Joep Driesen <joep@seasoning.be>
2015-07-06 14:58:15 +0200

    small stuff

--- Seasoning/Seasoning/urls.py
+++ Seasoning/Seasoning/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from recipes import urls as recipe_urls
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    
+    url(r'^$', lambda r: redirect('browse_recipes')),
     
     url(r'^recipes/', include(recipe_urls)),
 ]
