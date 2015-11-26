069fe46fb6ce5930191c98a1ff3d34d642a13c9f
Joep Driesen <joeper_100@hotmail.com>
2015-11-26 11:35:46 +0100

    infrastructure update

--- Seasoning/administration/management/commands/setup_infrastructure.py
+++ Seasoning/administration/management/commands/setup_infrastructure.py
@@ -15,18 +15,31 @@ class Command(NoArgsCommand):
             print('/etc/cron.daily does not exist. Cannot install cron scripts')
             return
         
-        with open('/etc/cron.daily/seasoning_backup_db.sh', 'w+') as f:
+        backup_db_script = '/etc/cron.daily/seasoning_backup_db.sh'
+        with open(backup_db_script, 'w+') as f:
             f.write(render_to_string('infrastructure/cron/backup_db.sh.template', context={'settings': settings}))
-            
+        make_executable(backup_db_script)
+        
         print('Updated Database backup script at /etc/cron.daily/seasoning_backup_db.sh')
         
-        with open('/etc/cron.daily/seasoning_backup_media.sh', 'w+') as f:
+        backup_media_script = '/etc/cron.daily/seasoning_backup_media.sh'
+        with open(backup_media_script, 'w+') as f:
             f.write(render_to_string('infrastructure/cron/backup_media.sh.template', context={'settings': settings}))
-            
+        make_executable(backup_media_script)
+        
         print('Updated Media Files backup script at /etc/cron.daily/seasoning_backup_media.sh')
         
-        with open('/etc/cron.monthly/seasoning_update.sh', 'w+') as f:
+        update_seasoning_script = '/etc/cron.monthly/seasoning_update.sh'
+        with open(update_seasoning_script, 'w+') as f:
             f.write(render_to_string('infrastructure/cron/update.sh.template', context={'PYTHON_EXE': sys.executable,
                                                                                         'settings': settings}))
+        make_executable(update_seasoning_script)
             
         print('Updated Update script at /etc/cron.monthly/seasoning_update.sh')
+        
+
+
+def make_executable(filename):
+    mode = os.stat(filename).st_mode
+    mode |= (mode & 0o444) >> 2 # Copy R bits to X
+    os.chmod(filename, mode)
