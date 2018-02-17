5feaa4c4208a99e3a7dcc5a3d0f33e896314a413
Joep Driesen <joep.driesen@seasoning.be>
2018-02-17 16:12:49 +0100

    base project

--- client/admin/input/multipleInput.js
+++
@@ -1,9 +0,0 @@
-import { Field } from 'redux-form';
-const LatLngInput = () => (
-    <span>
-        <Field name="lat" component="input" type="number" placeholder="latitude" />
-        &nbsp;
-        <Field name="lng" component="input" type="number" placeholder="longitude" />
-    </span>
-);
-export default LatLngInput;
--- client/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-
-import App from './admin/Admin'
-
-
-render(<App />, document.getElementById('root'));
